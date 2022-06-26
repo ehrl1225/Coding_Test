7bc04dff9d2fc8632d8fb1a58c895a3cc6281a65
ehrl1235 <wjdwntls1225@naver.com>
2022-06-26 18:32:05 +0900

    added some

--- 입국심사.py
+++ 입국심사.py
@@ -5,8 +5,11 @@ def search(min_n,n,times):
     time = 1
     while n>count(min_n+time-1, times):
         print(min_n+time-1)
-        if n>count(min_n+time-1,times):
+        counted=count(min_n+time-1,times)
+        if n>counted:
             min_n=min_n+time-1
+        if n == counted:
+            break
         time*=2
     if n==count(min_n+time-1, times):
         return min_n+time-1
@@ -17,7 +20,7 @@ def solution(n,times):
     return answer
 
 if __name__ == '__main__':
-    n=10
+    n=6
     times = [7,10]
     # print(count(10,times))
     print(solution(n,times))
